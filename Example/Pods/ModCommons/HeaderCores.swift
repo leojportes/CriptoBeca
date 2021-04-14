cfa1f837c95cbbbed1188a041ab73f9b46c5871f
leojportes <leojportes@gmail.com>
2021-04-14 13:26:06 -0300

    HeaderColor - Componente de Cores

---
+++ Example/Pods/ModCommons/HeaderCores.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderCores.swift
+//  ModCommons
+//
+//  Created by Leonardo Oliveira Portes on 14/04/21.
+//
+
+import Foundation
+
+
+public struct HeaderCores {
+    public static var headerColor: UIColor  { return UIColor(red: 0.55, green:0.59, blue:0.37, alpha: 1) }
+}
